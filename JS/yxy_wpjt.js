4c2882e0d0f81f7e38d459922645bd12e862ec5f
joyoner <joyoner@gmail.com>
2019-11-06 21:42:04 +0800

    Update yxy_wpjt.js

--- JS/yxy_wpjt.js
+++ JS/yxy_wpjt.js
@@ -3,7 +3,7 @@
 [Script]
 http-response https:\/\/yxy.*\.drcuiyutao\.com.*lecture\/detail requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/joyoner/rule/master/JS/yxy_wpjt.js
 [MITM]
-hostname = drcuiyutao.com
+hostname = yxyapi*.drcuiyutao.com
 */
 
 body = $response.body.replace(/\"type\":1/g, "\"type\":0");
