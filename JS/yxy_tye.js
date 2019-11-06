9d0f0172c23fc8c3ffdd650ba3e5ee9eb45a7cdb
joyoner <joyoner@gmail.com>
2019-11-06 21:18:59 +0800

    Update yxy_tye.js

--- JS/yxy_tye.js
+++ JS/yxy_tye.js
@@ -1,7 +1,7 @@
 /*
 育学园-谈育儿
 [Script]
-http-response https:\/\/yxy.*\.drcuiyutao\.com.*getVipAudioDetailById requires-body=1,max-size=262144,script-path=yxy_tye.js
+http-response https:\/\/yxy.*\.drcuiyutao\.com.*getVipAudioDetailById requires-body=1,max-size=262144,script-path=https://raw.githubusercontent.com/joyoner/rule/master/JS/yxy_tye.js
 [MITM]
 hostname = drcuiyutao.com
 */
