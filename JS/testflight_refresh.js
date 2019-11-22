b4c2636affd3a4ee5f42005ae5e4d99969226ab3
joyoner <joyoner@gmail.com>
2019-11-22 21:29:53 +0800

    Update testflight_refresh.js

--- JS/testflight_refresh.js
+++ JS/testflight_refresh.js
@@ -13,7 +13,7 @@ $httpClient.get(url, (error, response, data) => {
     //console.log(data)
 
     if (data.match('This beta is full') || data.match('测试员已满')) {
-      //$notification.post('Full','Full','Full')
+      $notification.post('Full','Full','Full')
     } else {
       $notification.post('Open', 'Join', `itms-beta://testflight.apple.com/join/${appid}`)
     }
